141efd4fad2d3da6aceb2ada112ac1e44540aa3c
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2022-11-19 12:33:45 +0100

    Better split of configuration options

--- build.sh
+++ build.sh
@@ -13,14 +13,14 @@ ISL_VERSION=0.18
 ZSTD_VERSION=1.4.9
 BINUTILS_VERSION=2.36
 GCC_VERSION=8.4.0
-#GCC_VERSION=10.3.0
+GCC_VERSION=10.3.0
 MINGW64_VERSION=7.0.0
 GDB_VERSION=10.2
 
 # Default folders
 SRC_DIR="$(pwd)/sources"
 WRK_DIR="$(pwd)/workdir"
-JOBS="$(nproc)"
+JOBS=$(( $(nproc) * 2 ))
 #JOBS=1
 LD_LIBRARY_PATH=""
 HOST_SYSROOT=""
@@ -111,6 +111,7 @@ build_package() {
 }
 
 build_toolchain() {
+	multilib=true
 	shared=false
 	figlet "$1" -w 140
 	figlet " -> "
@@ -133,12 +134,16 @@ build_toolchain() {
 	fi
 	export LDFLAGS="-L$HOST_SYSROOT/lib/gcc/$1/lib"
 	HOST_OPTIONS="$BASE_OPTIONS"
+	# GMP
 	build_package gmp "$SRC_DIR/gmp-${GMP_VERSION}" "$HOST_OPTIONS $LIBS_OPTIONS"
 	HOST_OPTIONS="$HOST_OPTIONS --with-gmp=$SYSROOT"
+	# MPFR
 	build_package mpfr "$SRC_DIR/mpfr-${MPFR_VERSION}" "$HOST_OPTIONS $LIBS_OPTIONS"
 	HOST_OPTIONS="$HOST_OPTIONS --with-mpfr=$SYSROOT"
+	# MPC
 	build_package mpc "$SRC_DIR/mpc-${MPC_VERSION}" "$HOST_OPTIONS $LIBS_OPTIONS"
 	HOST_OPTIONS="$HOST_OPTIONS --with-mpc=$SYSROOT"
+	# ISL
 	if [ "$1" = "x86_64-w64-mingw32" ]; then
 		if [ "$shared" = true ]; then
 			MAKE_FLAGS="LDFLAGS=-no-undefined"
@@ -147,28 +152,41 @@ build_toolchain() {
 	build_package isl "$SRC_DIR/isl-${ISL_VERSION}" "$BASE_OPTIONS $LIBS_OPTIONS --with-sysroot=$SYSROOT --with-gmp-prefix=$SYSROOT"
 	MAKE_FLAGS=""
 	HOST_OPTIONS="$HOST_OPTIONS --with-isl=$SYSROOT"
-	TARGET_OPTIONS="$HOST_OPTIONS --with-sysroot=$SYSROOT  --target=$2 --disable-nls --enable-version-specific-runtime-libs "
-	#TARGET_OPTIONS="$TARGET_OPTIONS"
-	#MINGW_OPTIONS="$HOST_OPTIONS --prefix=$SYSROOT/$2 --enable-lib32 --enable-lib64 --enable-wildcard --host=$2"
-	MINGW_OPTIONS="$HOST_OPTIONS --prefix=$SYSROOT/$2 --enable-lib32 --enable-lib64 --host=$2"
+	# Prepare options
+	TARGET_OPTIONS="$HOST_OPTIONS --with-sysroot=$SYSROOT --target=$2 --disable-nls"
+	BINUTILS_OPTIONS="$TARGET_OPTIONS"
+	GCC_OPTIONS="$TARGET_OPTIONS --enable-languages=c,c++"
+	MINGW_CRT_OPTIONS="--with-sysroot=$SYSROOT/$2 --prefix=$SYSROOT/$2 --host=$2"
+	if [ "multilib" = true ]; then
+		BINUTILS_OPTIONS="$BINUTILS_OPTIONS --enable-targets=x86_64-w64-mingw32,i686-w64-mingw32"
+		GCC_OPTIONS="$GCC_OPTIONS --enable-targets=all"
+		MINGW_CRT_OPTIONS="$MINGW_CRT_OPTIONS --enable-lib32"
+	fi
+	#GCC_OPTIONS="$GCC_OPTIONS --enable-threads=posix"
+	if [ "$shared" = true ]; then
+		TARGET_OPTIONS="$TARGET_OPTIONS --enable-multilib --enable-version-specific-runtime-libs --enable-sjlj-exceptions"
+	fi
+	# MinGW headers
 	case $2 in
 	*"mingw32"*)
-		TARGET_OPTIONS="$TARGET_OPTIONS --enable-targets=x86_64-w64-mingw32,i686-w64-mingw32"
-		build_package mingw_headers "$SRC_DIR/mingw-w64-v${MINGW64_VERSION}/mingw-w64-headers" "$MINGW_OPTIONS"
+		build_package mingw_headers "$SRC_DIR/mingw-w64-v${MINGW64_VERSION}/mingw-w64-headers" "-build=$1 --host=$2 --prefix=$SYSROOT/$2"
 		ln -s -f $SYSROOT/$2 $SYSROOT/mingw
 		mkdir -p $SYSROOT/$2/lib
 		ln -s -f $SYSROOT/$2/lib $SYSROOT/$2/lib64
 		;;
 	esac
-	build_package binutils "$SRC_DIR/binutils-${BINUTILS_VERSION}" "$TARGET_OPTIONS"
-	GCC_OPTIONS="$TARGET_OPTIONS --enable-languages=c,c++"
+	# Binutils
+	build_package binutils "$SRC_DIR/binutils-${BINUTILS_VERSION}" "$BINUTILS_OPTIONS"
+	# Gcc step 1
 	build_package gcc.step1 "$SRC_DIR/gcc-${GCC_VERSION}" "$GCC_OPTIONS" "all-gcc" "install-gcc"
+	# MinGW crt
 	case $2 in
 	*"mingw32"*)
-		build_package mingw_crt "$SRC_DIR/mingw-w64-v${MINGW64_VERSION}" "$MINGW_OPTIONS"
-		#build_package mingw_crt "$SRC_DIR/mingw-w64-v${MINGW64_VERSION}" "$MINGW_OPTIONS --with-libraries=winpthreads"
+		build_package mingw_crt "$SRC_DIR/mingw-w64-v${MINGW64_VERSION}/mingw-w64-crt" "$MINGW_CRT_OPTIONS"
+		#build_package mingw_pthreads "$SRC_DIR/mingw-w64-v${MINGW64_VERSION}/mingw-w64-libraries/winpthreads" "$MINGW_OPTIONS"
 		;;
 	esac
+	# GCC step 2
 	build_package gcc.step2 "$SRC_DIR/gcc-${GCC_VERSION}" "$GCC_OPTIONS"
 	HOST_SYSROOT="$SYSROOT"
 }
@@ -181,6 +199,8 @@ cd "$WRK_DIR"
 mkdir -p "$SRC_DIR"
 download
 
+figlet "JOBS=$JOBS"
+
 # Build toolchain for build->target
 build_toolchain "$CFG_BUILD" "$CFG_TARGET"
 
@@ -191,10 +211,10 @@ fi
 
 rm -f main.exe main.32.exe
 # Try in 64 bits
-wine64 x86_64-w64-mingw32-8.4.0/bin/gcc.exe ../main.c -o main.exe
+wine64 "x86_64-w64-mingw32-${GCC_VERSION}"/bin/gcc.exe ../main.c -o main.exe
 file main.exe
 wine64 main.exe
 # Try in 32 bits
-wine64 x86_64-w64-mingw32-8.4.0/bin/gcc.exe ../main.c -m32 -o main.32.exe
+wine64 "x86_64-w64-mingw32-${GCC_VERSION}"/bin/gcc.exe ../main.c -m32 -o main.32.exe
 file main.32.exe
 wine64 main.32.exe
