25194d268ac32b19e81e902d2bdc7728e1b803f0
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2024-02-09 12:28:04 +0100

    Reference current directory

--- docker_build_helper.sh
+++ docker_build_helper.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
+# Current script folder
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
 function prepare_container() {
 	docker rm -f gcc_multilib || true
-	cat "./scripts/Dockerfile.$1" "./scripts/Dockerfile.base" | docker build -t gcc_multilib -
+	cat "$SCRIPT_DIR/scripts/Dockerfile.$1" "$SCRIPT_DIR/scripts/Dockerfile.base" | docker build -t gcc_multilib -
 	docker run -d --name gcc_multilib gcc_multilib sleep 86400 # 24 hours...
-	docker cp patches gcc_multilib:/patches
-	docker cp scripts gcc_multilib:/scripts
-	docker cp test gcc_multilib:/test
+	docker cp "$SCRIPT_DIR/patches" gcc_multilib:/patches
+	docker cp "$SCRIPT_DIR/scripts" gcc_multilib:/scripts
+	docker cp "$SCRIPT_DIR/test" gcc_multilib:/test
 }
 
 function cleanup_container() {
